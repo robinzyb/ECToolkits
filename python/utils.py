a977a1087ac009be029865d86e1668fe6ee464ab
robinzhuang <38876805+robinzyb@users.noreply.github.com>
2020-04-26 15:43:00 +0200

    add cell scaling and process output

---
+++ python/utils.py
@@ -0,0 +1,6 @@
+def printtbox(arg):
+    """
+    This function is a print decorated with a few characters so that the
+    print function present a characteristic string. Useful for postprocess.
+    """
+    print("--> Toolkit: {0}".format(arg))

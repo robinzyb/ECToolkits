e8e50df718b2afdf52a9cc8f77a629d0211c8b2e
robinzyb <robinzhuang@outlook.com>
2022-04-22 11:00:58 +0800

    add version and remove useless file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="ECToolKits",
-    version="0.0.1",
+    version="0.1.1",
     author="Yongbin Zhuang",
     author_email="robinzhuang@outlook.com",
     description="Small Package to Postprocessing Results",

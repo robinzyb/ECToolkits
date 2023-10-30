dc01a06dbc902c93d5eaf95d0c8da2a89d5581ef
robinzyb <38876805+robinzyb@users.noreply.github.com>
2023-10-30 15:42:52 +0100

    New Feature: workflows for calculate dielectric constant

--- ectoolkits/cli/cmd.py
+++ ectoolkits/cli/cmd.py
@@ -1,9 +1,56 @@
 import click
+import yaml
+from ectoolkits.workflows.calc_diel import calc_diel
+
+# --- Helper functions ---
+def yaml_to_dict(yaml_file: str):
+    """
+    Convert a yaml file to a dictionary
+    """
+    with open(yaml_file, 'r') as fr:
+        input_dict = yaml.safe_load(fr)
+    return input_dict
+
+def batch_yaml_to_dict(input: str, 
+                       machine: str, 
+                       resources: str):
+    """
+    Convert a yaml file to a dictionary
+    """
+    input_dict = yaml_to_dict(input)
+    machine_dict = yaml_to_dict(machine)
+    resources_dict = yaml_to_dict(resources)
+    return input_dict, machine_dict, resources_dict
+# --- End of helper functions ---
 
 @click.group("cli")
 def cli():
     pass
 
-@click.group("wkflow")
+@cli.group("wkflow")
 def wkflow():
     click.echo('Performing ECToolkits Workflow')
+
+## --input_file put every into a yaml file! keep it simple
+## --machine
+## --resources
+@wkflow.command("calc_diel")
+@click.option('--input', '-i', 
+              type=click.Path(exists=True), help='Input file for workflow')
+@click.option('--machine', '-m', 
+              type=click.Path(exists=True), help='Machine to run the workflow on')
+@click.option('--resources', '-r',
+              type=click.Path(exists=True), help='Resources to use for the workflow')
+def calc_diel_cli(input, machine, resources):
+    input_dict, machine_dict, resources_dict = \
+        batch_yaml_to_dict(input, machine, resources)
+    scale = input_dict.pop("scale")
+    if scale == 'global':
+        calc_diel(**input_dict, 
+                machine_dict=machine_dict, 
+                resources_dict=resources_dict)
+    elif scale == 'atomic':
+        pass
+    else:
+        print(f"The scale {scale} is not supported!")
+# change the calc_diel function
---
+++ ectoolkits/workflows/calc_diel.py
@@ -0,0 +1,180 @@
+import numpy.typing as npt
+import numpy as np
+from typing import Dict
+from typing import List
+from pathlib import Path
+import shutil
+from dpdispatcher import Machine, Resources, Task, Submission
+from cp2k_input_tools.parser import CP2KInputParser, CP2KInputParserSimplified
+from cp2k_input_tools.generator import CP2KInputGenerator
+
+def copy_file_list(file_list, target_dir):
+    target_dir = Path(target_dir)
+    for file in file_list:
+        file = Path(file)
+        file_basename = file.name
+        src = file
+        dst = target_dir/file_basename
+        if src.is_dir():
+            shutil.copytree(src, dst, symlinks=True)
+            print(f"COPY directory {src}")
+            print(f"TO {dst}")
+        elif src.is_file():
+            shutil.copy2(src, dst)
+            print(f"COPY file {src}")
+            print(f"TO {dst}")
+
+def gen_cp2k_input_dict(input_file: str, 
+                        canonical: bool
+                        ):
+    if canonical:
+        parser = CP2KInputParser()
+    else:
+        parser = CP2KInputParserSimplified()
+    with open(input_file) as fhandle:
+        input_dict = parser.parse(fhandle)
+    return input_dict
+
+def write_cp2k_input(input_dict: Dict, 
+                     file_name: str
+                     ):
+    generator = CP2KInputGenerator()
+    with open(file_name, "w") as fhandle:
+        for line in generator.line_iter(input_dict):
+            fhandle.write(f"{line}\n")
+
+def add_efield_input(input_dict: Dict, 
+                     intensity: float,
+                     displacement_field: bool,
+                     polarisation: npt.NDArray[np.float64],
+                     d_filter: npt.NDArray[np.float64], 
+                     ):
+    
+    # Add the efield input to the input dictionary
+    assert len(input_dict['+force_eval']) == 1, \
+        "Only one FORCE_EVAL is supported for now"
+    input_dict['+force_eval'][0]['+dft']['+periodic_efield'] = {}
+    input_dict['+force_eval'][0]['+dft']['+periodic_efield']['intensity'] = intensity
+    input_dict['+force_eval'][0]['+dft']['+periodic_efield']['displacement_field'] = displacement_field
+    input_dict['+force_eval'][0]['+dft']['+periodic_efield']['polarisation'] = polarisation
+    input_dict['+force_eval'][0]['+dft']['+periodic_efield']['d_filter'] = d_filter
+
+    return input_dict
+
+def add_print_moments(input_dict: Dict,
+                      periodic: bool,
+                      filename: str,
+                      ):
+    # Add the print moments input to the input dictionary
+    assert len(input_dict['+force_eval']) == 1, \
+        "Only one FORCE_EVAL is supported for now"
+    input_dict['+force_eval'][0]['+dft']['+print']['+moments']['periodic'] = periodic
+    input_dict['+force_eval'][0]['+dft']['+print']['+moments']['filename'] = filename
+    return input_dict
+
+def add_run_type(input_dict: Dict,
+                 run_type: str,
+                 ):
+    # Add the run type input to the input dictionary
+    assert len(input_dict['+force_eval']) == 1, \
+        "Only one FORCE_EVAL is supported for now"
+    input_dict['+global']['run_type'] = run_type
+    return input_dict
+
+def gen_series_calc_efield(input_dict: Dict,
+                           intensity_array: npt.NDArray[np.float64],
+                           displacement: bool,
+                           polarisation: npt.NDArray[np.float64],
+                           d_filter: npt.NDArray[np.float64],
+                           periodic: bool,
+                           eps_type: str,
+                           filename: str,
+                           output_dir: str,
+                           extra_forward_files: List[str],
+                           ):
+    
+    # store the path for each calculation
+    task_work_path_list = []
+    # produce input files for each calculation
+    output_dir = Path(output_dir)
+
+    # Add the print moments input to the input dictionary
+    input_dict = add_print_moments(input_dict, periodic, filename)
+    if eps_type == "optical":
+        input_dict = add_run_type(input_dict, "ENERGY_FORCE")
+    elif eps_type == "static":
+        input_dict = add_run_type(input_dict, "GEO_OPT")
+
+    # Add the efield input to the input dictionary
+    for intensity in intensity_array:
+        input_dict = add_efield_input(input_dict, intensity, displacement, polarisation, d_filter)
+
+        # Write the input dictionary to a file
+        single_calc_dir = output_dir/f"efield_{intensity:7.6f}"
+        single_calc_dir.mkdir(parents=True, exist_ok=True)
+        task_work_path_list.append(str(single_calc_dir))
+
+        output_file = single_calc_dir/"input.inp"
+        write_cp2k_input(input_dict, output_file)
+        print(f"Input file for efield {intensity:7.6f} written to {output_file}")
+        copy_file_list(extra_forward_files, single_calc_dir)
+
+    return task_work_path_list
+
+def gen_task_list(command, task_work_path_list, extra_forward_files):
+    # generate task list
+    task_list = []
+    forward_files = extra_forward_files + ["input.inp"]
+    backward_files = ["*"]
+    outlog = "cp2k.log"
+    for task_work_path in task_work_path_list:
+        task = Task(command=command, 
+                    task_work_path=task_work_path,
+                    forward_files=forward_files,
+                    backward_files=backward_files,
+                    outlog=outlog)
+        task_list.append(task)
+    return task_list
+
+def calc_diel(input_file: str,
+              intensity_array: npt.NDArray[np.float64],
+              displacement_field: bool,
+              polarisation: npt.NDArray[np.float64],
+              d_filter: npt.NDArray[np.float64],
+              eps_type: str,
+              output_dir: str,
+              machine_dict: Dict,
+              resources_dict: Dict,
+              command: str,
+              extra_forward_files: List[str]=[],
+              extra_common_forward_files: List[str]=[],
+              ):
+    # gen input dict
+    template_input_dict = gen_cp2k_input_dict(input_file, canonical=True)
+    # gen task work path list
+    task_work_path_list = gen_series_calc_efield(template_input_dict, 
+                                                 intensity_array, 
+                                                 displacement_field, 
+                                                 polarisation, 
+                                                 d_filter, 
+                                                 periodic=True, 
+                                                 eps_type=eps_type,
+                                                 filename="=moments.dat", 
+                                                 output_dir=output_dir,
+                                                 extra_forward_files=extra_forward_files,
+                                                 )
+    # gen task
+    task_list = gen_task_list(command, task_work_path_list, extra_forward_files)
+    # submission
+    machine = Machine.load_from_dict(machine_dict)
+    resources = Resources.load_from_dict(resources_dict)
+    common_forward_files = extra_common_forward_files
+    submission = Submission(machine=machine, 
+                            resources=resources, 
+                            task_list=task_list, 
+                            common_forward_files=common_forward_files,
+                            backward_common_files=[],
+                            )
+    submission.run_submission()
+    
+    print("Workflow for Calculation of Dielectric Constant Complete!")

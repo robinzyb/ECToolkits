601d1dd27fa7bbb0d93962cbba36ea4ac728dc03
robinzyb <38876805+robinzyb@users.noreply.github.com>
2023-10-31 14:51:57 +0100

    update cmd for wkflow and finish calc_diel.py

--- ectoolkits/workflows/calc_diel.py
+++ ectoolkits/workflows/calc_diel.py
@@ -1,5 +1,6 @@
 import numpy.typing as npt
 import numpy as np
+from scipy.stats import linregress
 from typing import Dict
 from typing import List
 from pathlib import Path
@@ -7,8 +8,14 @@ import shutil
 from dpdispatcher import Machine, Resources, Task, Submission
 from cp2k_input_tools.parser import CP2KInputParser, CP2KInputParserSimplified
 from cp2k_input_tools.generator import CP2KInputGenerator
+from cp2kdata.block_parser.dipole import parse_dipole_list
+from cp2kdata import Cp2kOutput
+from cp2kdata.units import au2A
 
 DIPOLE_MOMENT_FILE = "moments.dat"
+CP2K_LOG_FILE = "cp2k.log"
+debey2au=4.26133088E-01/1.08312217E+00
+
 def copy_file_list(file_list, target_dir):
     target_dir = Path(target_dir)
     for file in file_list:
@@ -24,6 +31,40 @@ def copy_file_list(file_list, target_dir):
             shutil.copy2(src, dst)
             print(f"COPY file {src}")
             print(f"TO {dst}")
+
+def file_to_list(fname: str):
+    with open(fname, 'r') as fp:
+        output_file = fp.read()
+    return output_file
+
+def get_dipole_moment_array(task_work_path_list: List[str],
+                           output_dir: str,):
+    dipole_moment_array = []
+    for task_work_path in task_work_path_list:
+        output_dir = Path(output_dir)
+        output_file = file_to_list(output_dir/task_work_path/DIPOLE_MOMENT_FILE)
+        dipole_moment_array.append(parse_dipole_list(output_file)[0][3])
+    return np.array(dipole_moment_array)
+
+def get_volume_array(task_work_path_list: List[str],
+                     output_dir: str,): 
+    volume_array = []
+    for task_work_path in task_work_path_list:
+        output_dir = Path(output_dir)
+        cp2k_output = Cp2kOutput(output_dir/task_work_path/CP2K_LOG_FILE)
+        cell = cp2k_output.get_all_cells()[0]
+        volume = np.linalg.det(cell)/(au2A**3)
+        volume_array.append(volume)
+    return np.array(volume_array)
+
+def get_dielectric_constant(dipole_moment_array: npt.NDArray[np.float64],
+                            intensity_array: npt.NDArray[np.float64],
+                            volume_array: npt.NDArray[np.float64],):
+    
+    polarization_array = dipole_moment_array/volume_array
+    slope, intercept, r, p, se = linregress(intensity_array, polarization_array)
+    dielectric_constant = slope * 4 * np.pi + 1
+    return dielectric_constant
 
 def gen_cp2k_input_dict(input_file: str, 
                         canonical: bool
@@ -132,7 +173,7 @@ def gen_series_calc_efield(input_dict: Dict,
         # Write the input dictionary to a file
         single_calc_dir = output_dir/f"efield_{intensity:7.6f}"
         single_calc_dir.mkdir(parents=True, exist_ok=True)
-        # task_work_path should be relative to the work_base
+        # task_work_path should be relative to the work_base, i.e. output_dir
         task_work_path_list.append(single_calc_dir.name)
 
         output_file = single_calc_dir/"input.inp"
@@ -146,7 +187,7 @@ def gen_task_list(command, task_work_path_list, extra_forward_files):
     # generate task list
     task_list = []
 
-    outlog = "cp2k.log"
+    outlog = CP2K_LOG_FILE 
     forward_files = extra_forward_files + ["input.inp"]
     backward_files = [DIPOLE_MOMENT_FILE, outlog]
     for task_work_path in task_work_path_list:
@@ -171,6 +212,7 @@ def calc_diel(input_file: str,
               extra_forward_files: List[str]=[],
               extra_forward_common_files: List[str]=[],
               restart_wfn: str=None,
+              dry_run: bool=False,
               ):
     # gen input dict
     template_input_dict = gen_cp2k_input_dict(input_file, canonical=True)
@@ -187,7 +229,6 @@ def calc_diel(input_file: str,
                                                  extra_forward_files=extra_forward_files,
                                                  restart_wfn=restart_wfn
                                                  )
-    print(task_work_path_list)
     # gen task
     task_list = gen_task_list(command, task_work_path_list, extra_forward_files)
     # submission
@@ -209,6 +250,21 @@ def calc_diel(input_file: str,
                             forward_common_files=forward_common_files,
                             backward_common_files=[],
                             )
-    submission.run_submission()
-    
+    if dry_run:
+        # dry_run has been already true
+        exit_on_submit = True
+    submission.run_submission(dry_run=dry_run, exit_on_submit=exit_on_submit)
+
+    dipole_moment_array = get_dipole_moment_array(task_work_path_list, output_dir)
+    volume_array = get_volume_array(task_work_path_list, output_dir)
+    # use one volume for all calculation
+    dielectric_constant = get_dielectric_constant(dipole_moment_array, 
+                                                  intensity_array, 
+                                                  volume_array)
+    #
+    np.savetxt("dipole_moment_array.dat", dipole_moment_array)
+    np.savetxt("intensity_array.dat", intensity_array)
+    np.savetxt("volume_array.dat", volume_array)
+
+    print(f"The Dielectric Constant is {dielectric_constant:10.6f}")
     print("Workflow for Calculation of Dielectric Constant Complete!")

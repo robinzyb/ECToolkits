a45dc10d97aadb9c34577ba1c0495d1a2b9b8d59
robinzyb <38876805+robinzyb@users.noreply.github.com>
2025-02-09 16:33:44 +0100

    add plat_model_devi function

--- ectoolkits/plots/dpmd.py
+++ ectoolkits/plots/dpmd.py
@@ -1,6 +1,9 @@
+from typing import Tuple, Union, List
 import numpy.typing as npt
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+import cp2kdata.plots.colormaps
 
 
 def get_mae(data: npt.NDArray[np.float64],
@@ -13,7 +16,18 @@ def get_rmse(data: npt.NDArray[np.float64],
             ) -> float:
     return np.sqrt(np.mean((data - ref)**2))
 
-def get_dptest_data(e_file, f_file):
+def get_dptest_data(e_file:str,
+                    f_file:str
+                    ) -> Tuple[npt.NDArray[np.float64],
+                               npt.NDArray[np.float64],
+                               npt.NDArray[np.float64],
+                               npt.NDArray[np.float64],
+                               npt.NDArray[np.float64],
+                               npt.NDArray[np.float64],
+                               npt.NDArray[np.float64],
+                               npt.NDArray[np.float64],
+                               int
+                               ]:
     e_data, e_pred = np.loadtxt(e_file, unpack=True)
     e_mean = np.mean(e_data)
     e_data -= e_mean
@@ -33,7 +47,13 @@ def get_dptest_data(e_file, f_file):
 
 
 
-def plot_error(ax, data, pred, type='energy', title='Energy', return_err=False):
+def plot_error(ax,
+               data: npt.NDArray,
+               pred: npt.NDArray,
+               type: str='energy',
+               title: str='Energy',
+               return_err: bool=False
+               ) -> Union[None, Tuple[float, float]]:
 
 
     ax.scatter(data, pred, s=0.5)
@@ -65,7 +85,12 @@ def plot_error(ax, data, pred, type='energy', title='Energy', return_err=False):
     if return_err:
         return rmse, mae
 
-def plot_dptest(e_file: str, f_file: str, save_name: str="dptest.png", return_err: bool=False, frc_comp: bool=False):
+def plot_dptest(e_file: str,
+                f_file: str,
+                save_name: str="dptest.png",
+                return_err: bool=False,
+                frc_comp: bool=False
+                ):
 
     plt.style.use("cp2kdata.matplotlibstyle.jcp")
     if frc_comp:
@@ -114,3 +139,89 @@ def plot_dptest(e_file: str, f_file: str, save_name: str="dptest.png", return_er
         return fig
 
 
+## model deviation
+def _hist_and_stat_model_devi(model_devi_files: List[List[str]],
+                              trust_lo: float,
+                              trust_hi: float,
+                              **kwargs
+                              ) -> Tuple[List[Tuple[npt.NDArray, npt.NDArray]],
+                                         List[List[float]]
+                                         ]:
+    hist_model_devi = []
+    stat_model_devi = []
+    for _iter_files in  model_devi_files:
+        _model_devi = None
+        for _model_devi_file in _iter_files:
+            if _model_devi is None:
+                _model_devi = np.loadtxt(_model_devi_file, usecols=4)
+            else:
+                _model_devi = np.concatenate((_model_devi, np.loadtxt(_model_devi_file, usecols=4)))
+
+        ntotal = len(_model_devi)
+        naccurate = np.sum((_model_devi < trust_lo))
+        ncandidate = np.sum((_model_devi > trust_lo) &(_model_devi < trust_hi))
+        npoor = np.sum((_model_devi > trust_hi))
+
+        ratio_accurate = naccurate / ntotal
+        ratio_candidate = ncandidate / ntotal
+        ratio_poor = npoor / ntotal
+        stat_model_devi.append([ratio_accurate, ratio_candidate, ratio_poor])
+
+        hist, bin_edges = np.histogram(_model_devi, **kwargs)
+        hist_model_devi.append([hist, bin_edges])
+    return hist_model_devi, stat_model_devi
+
+def plot_model_devi(model_devi_files: List[List[str]],
+                    trust_lo: float,
+                    trust_hi: float,
+                    save_name: str="model_devi.png",
+                    **kwargs
+                    ):
+
+    hist_model_devi, stat_model_devi = _hist_and_stat_model_devi(model_devi_files, trust_lo, trust_hi, **kwargs)
+
+    # get the maximum value of histograms.
+    max_hist = 0
+    for _hist, _bin_edges in hist_model_devi:
+        if _hist.max() > max_hist:
+            max_hist = _hist.max()
+
+    plt.style.use('cp2kdata.matplotlibstyle.jcp')
+    niters = len(hist_model_devi)
+    cp2kdata_cb_lscmap = mpl.colormaps['cp2kdata_cb_lscmap']
+    plt.rcParams["axes.prop_cycle"] = plt.cycler("color", cp2kdata_cb_lscmap(np.linspace(0, 1, niters)))
+
+    row = niters
+    col = 1
+    fig = plt.figure(figsize=(3.37*col, 1.00*row), dpi=300, facecolor="white")
+    gs = fig.add_gridspec(row, col, hspace=0.0)
+    shifts = np.linspace(0, 10, niters)
+
+    for i_hist_model_devi, (_hist, _bin_edges) in enumerate(hist_model_devi):
+        ax = fig.add_subplot(gs[i_hist_model_devi])
+        ax.fill(_bin_edges[:-1], _hist, label=f"iter-{i_hist_model_devi:03d}", alpha=0.5, color=f"C{i_hist_model_devi}")
+        ax.axvline(trust_lo, color="black", linestyle="--", label="trust_lo")
+        ax.axvline(trust_hi, color="black", linestyle="--", label="trust_hi")
+        name = "Accurate:"
+        percent = stat_model_devi[i_hist_model_devi][0] * 100
+        percent = f"{percent:4.2f}"
+        percent = percent.zfill(5)  # I must turn a float into a string to pad zeros.
+        ax.text(1.35, 0.3, f"{name:>10} {percent}%", transform=ax.transAxes, ha="right")
+        name = "Candidate:"
+        percent = stat_model_devi[i_hist_model_devi][1] * 100
+        percent = f"{percent:4.2f}"
+        percent = percent.zfill(5)
+        ax.text(1.35, 0.2, f"{name:>10} {percent}%", transform=ax.transAxes, ha="right")
+        name = "Poor:"
+        percent = stat_model_devi[i_hist_model_devi][2] * 100
+        percent = f"{percent:4.2f}"
+        percent = percent.zfill(5)
+        ax.text(1.35, 0.1, f"{name:>10} {percent}%", transform=ax.transAxes, ha="right")
+        ax.set_ylim(-0.05, max_hist + 0.05)
+        ax.legend(bbox_to_anchor=(1.37, 1.00))
+    # I prefer fig.supxlabel to ax.set_xlabel, but the y position of fig.supxlabel is scaled with the figure size.
+    ax.set_xlabel("Model deviation " + r"(eV/$\mathrm{\AA}$)")
+    fig.supylabel("Density")
+    if save_name:
+        fig.savefig(save_name, bbox_inches="tight")
+    return fig

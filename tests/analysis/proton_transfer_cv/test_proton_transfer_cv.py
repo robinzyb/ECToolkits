40e402192f4338b24a1b5617eba982da0280f765
robinzyb <38876805+robinzyb@users.noreply.github.com>
2025-03-10 12:16:25 +0100

    add pytest for indirect transfer

--- tests/analysis/proton_transfer_cv/test_proton_transfer_cv.py
+++ tests/analysis/proton_transfer_cv/test_proton_transfer_cv.py
@@ -10,7 +10,7 @@ from ectoolkits.analysis.proton_transfer_cv import ProtonTransferCV
 
 
 path_prefix = Path("tests/analysis/proton_transfer_cv/")
-system_list = ["bivo4-direct-pt",]
+system_list = ["bivo4-direct-pt", "bivo4-indirect-pt",]
 
 @pytest.fixture(params=system_list, ids=system_list, scope='class')
 def analysis_and_reference(request):
@@ -30,20 +30,47 @@ def analysis_and_reference(request):
     atomgroup = u.atoms
 
     # read types
-    idxs_type1_o = input_param["idxs_type1_o"]
-    idxs_type2_o = input_param["idxs_type2_o"]
-    analysis = ProtonTransferCV(atomgroup=atomgroup, idxs_type1_o=idxs_type1_o, idxs_type2_o=idxs_type2_o)
-    analysis.run()
+    idxs_type1_o = input_param.get("idxs_type1_o", None)
+    idxs_type2_o = input_param.get("idxs_type2_o", None)
+    num_bridge = input_param.get("num_bridge", None)
+    idxs_water_o = input_param.get("idxs_water_o", [])
 
+    input_dict = {
+        "atomgroup": atomgroup,
+        "idxs_type1_o": idxs_type1_o,
+        "idxs_type2_o": idxs_type2_o,
+        "num_bridge": num_bridge,
+        "idxs_water_o": idxs_water_o
+    }
+
+    # analysis = ProtonTransferCV(atomgroup=atomgroup, 
+    #                             idxs_type1_o=idxs_type1_o, 
+    #                             idxs_type2_o=idxs_type2_o,
+    #                             num_bridge=num_bridge,
+    #                             idxs_water_o=idxs_water_o
+    #                             )
+    # analysis.run()
     # reference
     reference_file = system_dir/"cv_reference.npy"
     reference = np.load(reference_file)
-    return analysis, reference
+    return input_dict, reference
 
 class TestProtonTransferCV():
-    def test_proton_transfer_cv(self, analysis_and_reference):
-        analysis = analysis_and_reference[0]
+    def test_proton_transfer_cv_results(self, analysis_and_reference):
+        input_dict = analysis_and_reference[0]
+        analysis = ProtonTransferCV(**input_dict)
+        analysis.run()
         ptcv_ref = analysis_and_reference[1]
+        ptcv = analysis.results
+        np.testing.assert_array_equal(ptcv, ptcv_ref)
+    
+    def test_extra_detail_false(self, analysis_and_reference):
+        input_dict = analysis_and_reference[0]
+        input_dict["extra_detail"] = False
+        analysis = ProtonTransferCV(**input_dict)
+        analysis.run()
+
+        ptcv_ref = analysis_and_reference[1][:, :3]
         ptcv = analysis.results
         np.testing.assert_array_equal(ptcv, ptcv_ref)
 

9ad33ba8f149b1abf73885a1224fd0cc31095010
robinzyb <38876805+robinzyb@users.noreply.github.com>
2025-03-14 17:09:39 +0100

    pytest for reweighting property

--- tests/analysis/reweight/gen_ref.py
+++ tests/analysis/reweight/gen_ref.py
@@ -1,11 +1,15 @@
 from pathlib import Path
 import numpy as np
-from ectoolkits.analysis.reweight import calc_FES
+from ectoolkits.analysis.reweight import calc_FES, calc_property_surface
 
 
-case_list = ["00.case_2d_fes"]
-for case in case_list:
+case_fes_list = ["00.case_2d_fes"]
+for case in case_fes_list:
     case = Path(case)
+    ref_file = case/"fes_ref.dat"
+    if ref_file.exists():
+        print("Reference file exists, skip")
+        continue
     colvar_file = case/"COLVAR"
     cv1, cv2, bias = np.loadtxt(colvar_file, usecols=(1, 2, 3), unpack=True)
 
@@ -13,7 +17,6 @@ for case in case_list:
     grid_bin_y = 10
     T = 350
     kbT = T*0.0083144621 # kJ/mol
-    bias = bias/kbT # to dimensionless
     cv1_min = 2.3
     cv1_max = 2.4
     cv2_min = 1.0
@@ -30,10 +33,51 @@ for case in case_list:
                 nbins_y=grid_bin_y,
                 kbT=kbT,
                 save_file=True,
-                name_file=case/"fes_ref.dat",
+                name_file=ref_file,
                 min_cv_x=cv1_min,
                 max_cv_x=cv1_max,
                 min_cv_y=cv2_min,
                 max_cv_y=cv2_max,
                 dim=2)
+    
+
+case_prop_list = ["01.case_2d_property"]
+for case in case_prop_list:
+    case = Path(case)
+    ref_file = case/"prop_ref.dat"
+    if ref_file.exists():
+        print("Reference file exists, skip")
+        continue
+    colvar_file = case/"COLVAR"
+    cv1, cv2, bias = np.loadtxt(colvar_file, usecols=(1, 2, 3), unpack=True)
+
+    prop_data = np.load(case/"property_data.npy")
+    grid_bin_x = 10
+    grid_bin_y = 10
+    T = 350
+    kbT = T*0.0083144621 # kJ/mol
+
+    cv1_min = 2.3
+    cv1_max = 2.4
+    cv2_min = 1.0
+    cv2_max = 2.0
+    bw_cv1 = 0.008
+    bw_cv2 = 0.008
+
+    fes = calc_property_surface(cv_x=cv1,
+                                cv_y=cv2,
+                                prop=prop_data,
+                                bias=bias,
+                                bandwidth_x=bw_cv1,
+                                bandwidth_y=bw_cv2,
+                                nbins_x=grid_bin_x,
+                                nbins_y=grid_bin_y,
+                                kbT=kbT,
+                                save_file=True,
+                                name_file=ref_file,
+                                min_cv_x=cv1_min,
+                                max_cv_x=cv1_max,
+                                min_cv_y=cv2_min,
+                                max_cv_y=cv2_max,
+                                dim=2)
 
